ab83d61911972eb98e581e3db02f767af0f736aa
Miryoqub006 <158826997+Miryoqub006@users.noreply.github.com>
2026-03-07 17:29:08 +0500

    Update QuestionController.cs

--- ExamPdp3/TestProject.Api/Controllers/QuestionController.cs
+++ ExamPdp3/TestProject.Api/Controllers/QuestionController.cs
@@ -35,26 +35,6 @@ public class QuestionController : Controller
 
 
 
-[Route("api/auth")]
-[ApiController]
-public class AuthController : ControllerBase
-{
-    private readonly IAuthService AuthService;
-    public AuthController()
-    {
-        AuthService = new AuthService();
-    }
 
-    [HttpPost("register")]
-    public Guid Register(UserRegisterDto userRegisterDto)
-    {
-        return AuthService.RegisterUser(userRegisterDto);
-    }
 
-    [HttpPost("login")]
-    public string Login(UserLoginDto userLoginDto)
-    {
-        return AuthService.LoginUser(userLoginDto);
-    }
-}
 
